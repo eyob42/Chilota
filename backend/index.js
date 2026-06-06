2ecff9cb99702a4e3a7fc14af22ea6af45d52a52
Eyob <eyob@gmail.com>
2026-06-06 21:02:47 +0300

    initial backend setup with express

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+console.log('server starting...')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/',(req, res)=> {
+    res.send('Chilota API is running!');
+});
+
+app.listen(PORT, () => {
+    console.log(`app listening  on port ${PORT}`);
+});
