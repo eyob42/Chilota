8422e12fc57438dd745ed01fa7a59d796510e969
Eyob <eyob@gmail.com>
2026-06-20 16:17:19 +0300

    update the auth email

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,7 +1,19 @@
-
-const register = (req, res)=> {
+const pool = require('../../db');
+const register =async (req, res)=> {
     const { password , email , role} = req.body
 
+    const checkEmailQuery = 'SELECT id FROM users WHERE email = $1;';
+    const result = await pool.query(checkEmailQuery, [email]);
+
+    if(result.rows.length > 0 ) {
+        return res.status(400).json({
+            error: 'Email already exists'
+        });
+    }
+
+    res.status(200).json({
+        message: 'Email is available'
+    })
 
     res.json({ password , email , role})
 }
